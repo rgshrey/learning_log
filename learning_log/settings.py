52bc13c3cbc550d83f243bc14985a972f6820cc9
rgshrey <rgshrey@gmail.com>
2017-07-08 02:08:52 -0400

    ran collectstatic locally

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/ryan/learning_log/learning_log/static/'
 
 # My settings
 LOGIN_URL = '/users/login/'
